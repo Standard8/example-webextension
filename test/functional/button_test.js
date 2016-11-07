07cc6e01a71dfcfffccc9cdbbab90aa4aaabccfd
Mark Banner <standard8@mozilla.com>
2016-11-07 11:31:40 +0000

    Move test/.eslintrc to be a .js file, and move a relevant comment from the test file to the config file.

--- test/functional/button_test.js
+++ test/functional/button_test.js
@@ -5,11 +5,6 @@ let utils = require("./utils");
 let firefox = require("selenium-webdriver/firefox");
 let Context = firefox.Context;
 
-// Mocha can't use arrow functions as sometimes we need to call `this` and
-// using an arrow function alters the binding of `this`.
-// Hence we disable prefer-arrow-callback here so that mocha/no-mocha-arrows can
-// be applied nicely.
-
 describe("Example Add-on Functional Tests", function() {
   // This gives Firefox time to start, and us a bit longer during some of the tests.
   this.timeout(10000);
